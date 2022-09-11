c3365ee40d7871338641210f98fa63fb7ed0d6eb
tsubokura <repeat2639@gmail.com>
2022-09-11 11:17:37 +0900

    sass-loader & sass

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,16 @@ module.exports = {
   module: {
     rules:[
       {
-        test: /\.css/,
+        test: /\.(css|sass|scss)/,
         use:[
           {
             loader: MiniCssExtractPlugin.loader,
           },
           {
             loader:'css-loader',
+          },
+          {
+            loader: 'sass-loader',
           },
         ],
       },
@@ -62,6 +65,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/templates/access.pug',
       filename: 'access.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/members/taro.pug',
+      filename: 'members/taro.html',
     }),
     new CleanWebpackPlugin(),
   ]
